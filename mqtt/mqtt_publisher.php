4902d0424ef9df109cc0e241adf5b8d7212d7239
adhitya <adhitya.aripamadhan@gmail.com>
2026-04-11 11:29:49 +0700

    Update mqtt_publisher.php

--- mqtt/mqtt_publisher.php
+++ mqtt/mqtt_publisher.php
@@ -6,35 +6,38 @@ use PhpMqtt\Client\MqttClient;
 
 $config = require __DIR__ . '/../config/mqtt.php';
 
+// buat client mqtt - broker, port, client id
 $mqtt = new MqttClient(
     $config['broker'],
     $config['port'],
     'publisher-' . uniqid()
 );
 
+// koneksi ke broker
 $mqtt->connect(null, true);
 
-// TEST LCD
+// tes lcd format bari1|baris2
 $mqtt->publish(
     $config['prefix'] . '/' . $config['topic_lcd'],
     'TEST LCD|OK',
     0
 );
 
-// TEST ENTRY
+// tes servo entry (palang masuk)
 $mqtt->publish(
     $config['prefix'] . '/' . $config['topic_entry_servo'],
     'OPEN',
     0
 );
 
-// TEST EXIT
+// tes servo exit (palang keluar)
 $mqtt->publish(
     $config['prefix'] . '/' . $config['topic_exit_servo'],
     'OPEN',
     0
 );
 
+// diskonek broker setelah tes selesai
 $mqtt->disconnect();
 
 echo "MQTT TEST SUCCESS\n";
