ceba72351ee387d247e57294083af899900a1bb8
adhitya <adhitya.aripamadhan@gmail.com>
2026-04-11 11:28:28 +0700

    Update User.php

--- app/models/User.php
+++ app/models/User.php
@@ -10,16 +10,23 @@ class User {
         $this->conn = $db->connect();
     }
 
+    // fungsi login
     public function login($username, $password) {
+        // query untuk mencari user berdasarkan username dan password 
         $query = "SELECT * FROM {$this->table} 
                   WHERE username = :username AND password = :password 
                   LIMIT 1";
 
+        // prepare statment untuk mencegah sql injection
         $stmt = $this->conn->prepare($query);
+
+        // binding parameter username dan password ke query
         $stmt->bindParam(":username", $username);
         $stmt->bindParam(":password", $password);
+
         $stmt->execute();
 
+        // mengambil hasil data user (jika ditemukan)
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 }
